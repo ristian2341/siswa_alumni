8c86cd43a92412abe29bc040f003dbedde5c3317
ristian.ekahendi@duakelinci.co.id <ristian.ekahendi@duakelinci.co.id>
2024-06-07 17:11:26 +0700

    commit

--- config/web.php
+++ config/web.php
@@ -14,7 +14,8 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'enableCookieValidation' => false,
+            'enableCsrfValidation' => false,
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
